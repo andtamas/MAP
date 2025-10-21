e241f761fe58617c7d867b1ba88f8e6dfe510210
andtamas <andrei.gabriel.tamas@stud.ubbcluj.ro>
2025-10-21 12:41:08 +0300

    Einfuegen der Main Datei

---
+++ Lab4/A1/src/gradesManager.java
@@ -0,0 +1,55 @@
+public class gradesManager {
+    public static int[] failingGrades(int[] grades) {
+        int count = 0;
+        int[] failing = new int[grades.length];
+
+        for (int i=0; i<grades.length; i++) {
+            if (grades[i]<38) {
+                failing[i] = grades[i];
+                count++;
+            }
+        }
+        int[] result = new int[count];
+        for (int i=0; i<count; i++) {
+            result[i] = failing[i];
+        }
+        return result;
+    }
+
+    public static double averageGrade(int[] grades) {
+        int sum = 0;
+        for (int i=0; i<grades.length; i++) {
+            sum += grades[i];
+        }
+        double result = sum/grades.length;
+        return result;
+    }
+
+    public static int[] roundedGrades(int[] grades) {
+        int[] result = new int[grades.length];
+        for (int i=0; i<grades.length; i++) {
+            if (grades[i] % 5 != 0) {
+                int nextMultiple5 = (grades[i]/5 + 1) * 5;
+                if (nextMultiple5 - grades[i] < 3) {
+                    result[i] = nextMultiple5;
+                } else {
+                    result[i] = grades[i];
+                }
+            } else {
+                result[i] = grades[i];
+            }
+        }
+        return result;
+    }
+
+    public static int highestRoundedGrade(int[] grades) {
+        int[] roundedGrades = roundedGrades(grades);
+        int max = 0;
+        for (int i=0; i<roundedGrades.length; i++) {
+            if (roundedGrades[i] > max) {
+                max = roundedGrades[i];
+            }
+        }
+        return max;
+    }
+}
---
+++ Lab4/A1/src/main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        int[] grades = {29,37,38,41,84,67};
+        int[] fail = gradesManager.failingGrades(grades);
+        double average = gradesManager.averageGrade(grades);
+        System.out.println("Nicht ausreichende Noten: " + Arrays.toString(fail));
+        System.out.println("Das Durchschnitt der Noten: " + );
+
+    }
+}
